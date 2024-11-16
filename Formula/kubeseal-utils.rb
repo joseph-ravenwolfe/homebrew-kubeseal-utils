b9e48f0d33941e9f6f555539f8ef68409b0706fb
Joseph Ravenwolfe <joseph@ravenwolfe.io>
2024-11-16 17:31:42 -0500

    Update for release v0.1.0

--- Formula/kubeseal-utils.rb
+++ Formula/kubeseal-utils.rb
@@ -1,8 +1,8 @@
 class KubesealUtils < Formula
   desc "Command line utility for working with Kubernetes sealed secrets"
   homepage "https://github.com/joseph-ravenwolfe/kubeseal-utils"
-  url "https://github.com/joseph-ravenwolfe/kubeseal-utils/archive/v0.1.0.tar.gz"
-  sha256 "THE_SHA_256_OF_YOUR_RELEASE_TARBALL"
+  url "https://github.com/joseph-ravenwolfe/homebrew-kubeseal-utils/archive/v0.1.0.tar.gz"
+  sha256 "99cf37749287f0e975aa633871fef39b00f7f709ff1ae4a62be835e05669d19f"
   license "MIT"
 
   depends_on "jq"
@@ -16,7 +16,7 @@ class KubesealUtils < Formula
   end
 
   test do
-    assert_match "Command line utility for working with Kubernetes sealed secrets",
-                 shell_output("#{bin}/kubeseal-utils --help")
+    output = shell_output("#{bin}/kubeseal-utils --help", 1)
+    assert_match "Usage:", output
   end
 end
