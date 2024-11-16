4530882e6c9830351ee92f4cdb2b902de7b336b4
Joseph Ravenwolfe <joseph@ravenwolfe.io>
2024-11-16 17:41:06 -0500

    Update for release v0.1.1

--- Formula/kubeseal-utils.rb
+++ Formula/kubeseal-utils.rb
@@ -1,8 +1,8 @@
 class KubesealUtils < Formula
   desc "Command line utility for working with Kubernetes sealed secrets"
   homepage "https://github.com/joseph-ravenwolfe/kubeseal-utils"
-  url "https://github.com/joseph-ravenwolfe/homebrew-kubeseal-utils/archive/v0.1.0.tar.gz"
-  sha256 "99cf37749287f0e975aa633871fef39b00f7f709ff1ae4a62be835e05669d19f"
+  url "https://github.com/joseph-ravenwolfe/homebrew-kubeseal-utils/archive/v0.1.1.tar.gz"
+  sha256 "cc4380c50f6f8b665287449591070eedb0e9bae8037fe55e52b6b1b17d33cf44"
   license "MIT"
 
   depends_on "jq"
